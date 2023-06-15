0c10da493b8c54860314b4770141c34d79565220
mr-eric199 <antwieric199@gmail.com>
2023-06-15 07:31:44 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point of the program
- *
- * Description: This function prints the message "and that piece of art is useful" - Dora Korpar, 2015-10-19”
- *
- * Return: Always 0 (success)
+ * main - Entry point
+ * Return:1
  */
-
 int main(void)
 {
-    write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
-
-    return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- *
- * Description: This function prints the message "Programming is like
- *              building a multilingual puzzle" and returns 0.
- *
- * Return: Always 0 (success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
