352eb68c9770bc8653eef7f83e9ae17dd6e49cbd
mr-eric199 <antwieric199@gmail.com>
2023-06-16 06:57:22 -0700

    2 file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+ *main- main function
+ *Return - return 0
+*/
 
 /* betty style doc for function main goes there */
 int main(void)
 {
 	int i;
 	char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
-	for (i = 0; i < 27; i++)
-    {
-        putchar(alphabet[i]);
-        putchar('\n');
-    }
 
-	return (0);
+	for (i = 0; i < 27; i++)
+	{
+		putchar(alphabet[i]);
+	}
+return (0);
 }
 
