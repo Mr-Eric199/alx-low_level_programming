ed6dafb33d9e29f341a7d79018dea4ac3488de74
mr-eric199 <antwieric199@gmail.com>
2023-06-16 08:26:31 -0700

    8 file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main- main function
+ * Return: return 0
+*/
 int main(void)
 {
 	int i;
 	char alphabetLow[27] = "abcdef";
+
 	for (i = 1; i <= 9; i++)
-    {
-        putchar('0' + i);
-        putchar('\n');
-    }
-    for (i = 0; i < 7; i++)
-    {
-        putchar(alphabetLow[i]);
-        putchar('\n');
-    }
-
-
-
+	{
+		putchar('0' + i);
+	}
+	for (i = 0; i < 7; i++)
+	{
+		putchar(alphabetLow[i]);
+	}
+	putchar('\n');
 	return (0);
 }
