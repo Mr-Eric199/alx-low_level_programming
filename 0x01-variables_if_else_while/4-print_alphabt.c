0764254fdf7c7b739b284aa866992d8c945a12f4
mr-eric199 <antwieric199@gmail.com>
2023-06-16 07:44:22 -0700

    4 fine

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main- main function
+ * Return: return 0
+*/
 int main(void)
 {
 	int i;
 	char alphabetLow[27] = "abcdefghijklmnopqrstuvwxyz";
-	for (i = 0; i < 27; i++)
-    {
-        if (alphabetLow[i] == 'q' || alphabetLow[i] == 'e' )
-        {
-            continue;
-        }
-        putchar(alphabetLow[i]);
-        putchar('\n');
-    }
-
-
-	return (0);
+	
+	for (i = 0; i < 26; i++)
+	{
+		if (alphabetLow[i] == 'q' || alphabetLow[i] == 'e' )
+		{
+			continue;
+		}
+		putchar(alphabetLow[i]);
+		putchar('\n');
+	}
+return (0);
 }
