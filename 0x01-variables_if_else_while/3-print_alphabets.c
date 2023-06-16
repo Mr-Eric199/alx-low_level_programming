26788a9ee7fab393592a8cccbe2b7893f75af1db
mr-eric199 <antwieric199@gmail.com>
2023-06-16 07:31:18 -0700

    3 file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/** more headers goes there
+ * main- main function
+ * Return- return o
+*/
 int main(void)
 {
 	char alphabetLow[27] = "abcdefghijklmnopqrstuvwxyz";
 	char alphabetUp[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+
 	for (i = 0; i < 27; i++)
-    {
-        putchar(alphabetLow[i]);
-        putchar('\n');
-    }
-    for (i = 0; i < 27; i++)
-    {
-        putchar(alphabetUp[i]);
-        putchar('\n');
-    }
-
-
+	{
+		putchar(alphabetLow[i]);
+		putchar('\n');
+	}
+	for (i = 0; i < 27; i++)
+	{
+		putchar(alphabetUp[i]);
+		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
 
