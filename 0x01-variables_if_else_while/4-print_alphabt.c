09aaae008edde2be2b6f6517af8a9c708e92629e
mr-eric199 <antwieric199@gmail.com>
2023-06-16 07:52:06 -0700

    4 fine

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i;
 	char alphabetLow[27] = "abcdefghijklmnopqrstuvwxyz";	
+	
 	for (i = 0; i < 26; i++)
 	{
-		if (alphabetLow[i] == 'q' || alphabetLow[i] == 'e' )
+		if (alphabetLow[i] == 'q' || alphabetLow[i] == 'e')
 		{
 			continue;
 		}
 		putchar(alphabetLow[i]);
-	
 	}
 	putchar('\n');
 return (0);
