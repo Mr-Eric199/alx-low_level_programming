399eb2836d2e3ea0df14cbb4b89d0b2bd4af8ac2
mr-eric199 <antwieric199@gmail.com>
2023-06-16 08:05:52 -0700

    5 file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main- main function
+ * Return: return 0
+*/
 int main(void)
 {
 	int i;
 	for (i = 0; i < 10; i++)
-    {
-        printf("%d\n", i);
-    }
-
+	{
+		printf("%d", i);
+	}
+	printf("\n");
 	return (0);
 }
